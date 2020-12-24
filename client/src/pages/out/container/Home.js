19961bab70c058fe1cee510fc812bad0ee88fb4a
stilig <stilig8715@gmail.com>
2020-12-24 21:57:42 +0800

    initial commit

---
+++ client/src/pages/out/container/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import "./Home.css"
+
+function Home() {
+    return ( 
+        <div style={{minHeight: "100vh"}}>
+            <div id = "Home_title">
+                NTUEE+
+            </div>
+        </div> 
+        )
+}
+
+export default Home
+
+
+
