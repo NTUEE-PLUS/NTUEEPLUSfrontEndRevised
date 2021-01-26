c951bdb0653092853accacc8090eaff8cddba305
stilig <stilig8715@gmail.com>
2021-01-26 15:14:00 +0800

    merge Navbar

--- client/src/pages/in/component/NavBar.js
+++ client/src/pages/in/component/NavBar.js
@@ -1,46 +1,20 @@
 import React from 'react'
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button'
-import Logo from './Logo'
-import '../css/NavBar.css'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import NavBar from '../../component/NavBar'
 
-const NavBar = () => {
+const navButton = [
+    { href: '/in/Career', buttonText: 'Career' },
+    { href: '/in/Study', buttonText: 'Study' },
+    { href: '/in/Column', buttonText: 'Column' },
+    { href: '/in/Search', buttonText: 'Search' },
+]
+
+const Nav = () => {
     return (
-        //=========================Bootstrap React =============================
-        <Navbar bg='light' expand='lg'>
-            <Navbar.Brand href='/in'>
-                <Logo />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav>
-                    <Nav.Link href='/in/Career'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Career
-                        </Button>
-                    </Nav.Link>
-                    <Nav.Link href='/in/Study'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Study
-                        </Button>
-                    </Nav.Link>
-                    <Nav.Link href='/in/Column'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Column
-                        </Button>
-                    </Nav.Link>
-                    <Nav.Link href='/in/Search'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Search
-                        </Button>
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <NavBar 
+            hasNavDropdown={false}
+            navButton={navButton}
+            navBrand='/in'
+        />
     )
 }
-export default NavBar
+export default Nav
--- client/src/pages/out/component/NavBar.js
+++ client/src/pages/out/component/NavBar.js
@@ -1,69 +1,26 @@
 import React from 'react'
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button'
-import Logo from './Logo'
-import '../css/NavBar.css'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from '../../component/NavBar'
+
+const navDropdown = [
+    { href: '/About', buttonText: 'About'},
+    { href: '/History', buttonText: 'History' },
+    { href: '/Team', buttonText: 'Team' },
+]
+
+const navButton = [
+    { href: '/Contact', buttonText: 'Contact' },
+    { href: '/Support', buttonText: 'Support' },
+    { href: '/Login', buttonText: 'Login' },
+]
 
 const NavBar = () => {
     return (
-        //=========================Bootstrap React =============================
-        <Navbar bg='light' expand='lg'>
-            <Navbar.Brand href='/'>
-                <Logo />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav>
-                    <Button id='AppBar_menuButton' className='noHoverBg'>
-                        <NavDropdown title='ABOUT' id='basic-nav-dropdown'>
-                            <NavDropdown.Item href='/About'>
-                                <Button
-                                    id='AppBar_menuButton'
-                                    className='noHoverBg'
-                                >
-                                    About
-                                </Button>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href='/History'>
-                                <Button
-                                    id='AppBar_menuButton'
-                                    className='noHoverBg'
-                                >
-                                    History
-                                </Button>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href='/Team'>
-                                <Button
-                                    id='AppBar_menuButton'
-                                    className='noHoverBg'
-                                >
-                                    Team
-                                </Button>
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Button>
-                    <Nav.Link href='/Contact'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Contact
-                        </Button>
-                    </Nav.Link>
-                    <Nav.Link href='/Support'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Support
-                        </Button>
-                    </Nav.Link>
-                    <Nav.Link href='/Login'>
-                        <Button id='AppBar_menuButton' className='noHoverBg'>
-                            Login
-                        </Button>
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <Nav 
+            hasNavDropdown={true}
+            navDropdown={navDropdown}
+            navButton={navButton}
+            navBrand='/'
+        />
     )
 }
 export default NavBar
