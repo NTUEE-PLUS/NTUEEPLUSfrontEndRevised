3d6a075dc3ab2709693116e043ef956d27c701e2
Jamesyu220 <james.yu220@gmail.com>
2021-02-28 14:55:00 +0800

    add the content of columns 0228 14:54

--- client/src/pages/in/component/ColumnContent.js
+++ client/src/pages/in/component/ColumnContent.js
@@ -34,164 +34,78 @@ const ColumnContent = (props) => {
 
 export default ColumnContent
 
-// import React, { Component } from 'react';
-// import './column_annotation.css';
-
 const Column_annotation = (props) => {
     const _annotation = props.annotation
     const _id = props.id
-    const addAnno = (i, content) => {
-        let tag_container = document.getElementById(_id)
-        let newtag = document.createElement('li')
-        newtag.setAttribute('href', '#')
-        newtag.setAttribute('class', 'column_annotation')
-        newtag.setAttribute('id', _id + '_' + i)
-        newtag.innerHTML = content + '\n'
-        tag_container.appendChild(newtag)
-    }
-    useEffect(() => {
-        _annotation.forEach((ann, i) => {
-            addAnno(i+1, ann)
-        })
-    }, [])
     return (
         <div id='Anno_space'>
-            <div id={_id} class='column_annotation'></div>
+            <div id={_id} className='column_annotation'>
+                {_annotation.map((content, i) => (
+                    <li 
+                        href='#'
+                        className='column_annotation'
+                        id={_id + '_' + i}
+                    >
+                        {content + '\n'}
+                    </li>
+                ))}
+            </div>
         </div>
     )
 }
-// export default Column_annotation;
-
-// import React, { Component } from 'react';
-// import Column_section from './column_section';
 
 const Column_article = (props) => {
     const _sections = props.sections
     const _id = props.id
     return (
         <div id={_id}>
-            {_sections.map((sec, i) => {
+            {_sections.map((content, i) => (
                 <Column_section
-                    id={_id + '_' + (i + 1)}
-                    sections={sec}
+                    id={_id+"_"+(i+1)} 
+                    sections = {_sections[i]} 
+                    key={_id+"_"+(i+1)}
                 />
-            })}
+            ))}
         </div>
     )
 }
-// export default Column_article;
-
-// import React, { Component } from 'react';
-// import './column_hashtags.css';
 
 const Column_hashtags = (props) => {
     const _hashtags = props.hashtags
     const _id = props.id
-    const addHashTag = (i, content) => {
-        let tag_container = document.getElementById(_id)
-        let newtag = document.createElement('a')
-        newtag.setAttribute('href', '#')
-        newtag.setAttribute('class', 'column_hashtags')
-        newtag.setAttribute('id', _id + '_' + i)
-        newtag.innerHTML = '#' + content
-        tag_container.appendChild(newtag)
-    }
-    useEffect(() => {
-        _hashtags.forEach((hag, i) => {
-            addHashTag(i+1, hag)
-        })
-    }, [])
-        
     return (
         <div id='hashtag_space'>
-            <div id={_id} class='column_hashtags'></div>
+            <div id={_id} class='column_hashtags'>
+                {_hashtags.map((content, i) => (
+                    <a 
+                        href='#' className='column_hashtags' 
+                        id={_id + '_' + (i+1)} 
+                        key={_id + '_' + (i+1)}
+                    >
+                        {'#' + content}
+                    </a>
+                ))}
+            </div>
         </div>
     )
 }
-// export default Column_hashtags;
-
-// import React, { Component } from 'react';
-// import './column_href.css';
-
-const Column_href = (props) => {
-    const _href = props.href
-    const _id = props.id
-    const addHref = (i, content) => {
-        let tag_container = document.getElementById(_id)
-        let newtag = document.createElement('a')
-        newtag.setAttribute('href', '#')
-        newtag.setAttribute('class', 'column_href')
-        newtag.setAttribute('id', _id + '_' + i)
-        newtag.innerHTML = '#' + content
-        tag_container.appendChild(newtag)
-    }
-    useEffect(() => {
-        _href.forEach((hr, i) => {
-            addHref(i+1, hr)
-        })
-    }, [])
-    return (
-        <div id='hrefspace'>
-            <div id={_id} class='column_href'></div>
-        </div>
-    )
-}
-// export default Column_href;
-
-// import React, { Component } from 'react';
-// import './column_section.css';
 
 const Column_section  = (props) => {
     const _bigtitle = props.sections.bigtitle
     const _sections = props.sections.sections
     const _id = props.id
-    useEffect(() => {
-        let minor_section_container = document.getElementById(_id)
-        _sections.forEach((sec, i) => {
-            let new_minor_section = document.createElement('div')
-            let minor_section_title = document.createElement('li')
-            let minor_section_content = document.createElement('p')
-            minor_section_title.innerHTML = sec.title
-            minor_section_title.setAttribute(
-                'id',
-                _id + '_minor_title' + (i + 1)
-            )
-            minor_section_title.setAttribute('class', 'column_minor_title')
-            minor_section_content.innerHTML =
-                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' +
-                sec.section
-            minor_section_content.setAttribute(
-                'id',
-                _id + '_content' + (i + 1)
-            )
-            minor_section_content.setAttribute('class', 'column_minor_content')
-            new_minor_section.setAttribute('class', 'column_minor_sections')
-            new_minor_section.setAttribute(
-                'id',
-                _id + '_minor_section' + (i + 1)
-            )
-
-            new_minor_section.appendChild(minor_section_title)
-            new_minor_section.appendChild(minor_section_content)
-            minor_section_container.appendChild(new_minor_section)
-            console.log("section "+i)
-        })
-    }, [])
     return (
         <div id={_id}>
-            <p
-                id={_id + '_bigtitle'}
-                class='column_sections_bigtitle'
-            >
-                {_bigtitle}
-            </p>
+            <p id={_id + '_bigtitle'} class='column_sections_bigtitle'>{_bigtitle}</p>
+            {_sections.map((_section, i) => (
+                <div className='column_minor_sections' id={_id + '_minor_section' + (i + 1)}>
+                    <li id={_id + '_minor_title' + (i + 1)} className='column_minor_title'>{_section.title}</li>
+                    <p id={_id + '_content' + (i + 1)} className='column_minor_content' >{_section.section}</p>
+                </div>
+            ))}
         </div>
     )
 }
-// export default Column_section;
-
-// import React, { Component } from 'react';
-// import './column_title.css';
 
 const Column_title  = (props) => {
     const _title = props.title
@@ -216,13 +130,3 @@ const Column_title  = (props) => {
         </div>
     )
 }
-
-// export default Column_title;
-
-// import React, { Component } from 'react';
-// import './column_sp_thx.css';
-
-const Column_sp_thx = (props) => {   
-    return <div id={props.id}>{props.sp_thx}</div>
-}
-// export default Column_sp_thx;
