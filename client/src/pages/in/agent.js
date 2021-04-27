1b09baa2d882b0bc84e55f686f7936ac680a6342
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-04-27 16:34:49 +0800

    agent done

--- client/src/pages/in/agent.js
+++ client/src/pages/in/agent.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ROOT = 'https://eeplus-back.herokuapp.com/api/'//'http://localhost:1993/api'//
+const API_ROOT = 'http://localhost:1993/api'//'https://eeplus-back.herokuapp.com/api/'//
 const instance = axios.create({
   baseURL: API_ROOT,
   withCredentials: true
@@ -18,6 +18,20 @@ const study_matching = async ({senior,junior}) => {
     toSend.append('junior',junior)
     return await instance.post(
         '/study_matching',
+        toSend,
+        { 
+            "content-type": "multipart/form-data",
+            responseType: 'blob' 
+        }
+    )
+}
+const matchResult = async (file)=>{
+    await instance.post('/login',{account:'b07901029',password:'123'})
+    console.log('login success')
+    const toSend = new FormData()
+    toSend.append('senior',senior)
+    return await instance.post(
+        '/',
         toSend,
         { 
             "content-type": "multipart/form-data",
--- client/src/pages/in/component/StudyBlock.js
+++ client/src/pages/in/component/StudyBlock.js
@@ -28,18 +28,63 @@ export const Match = () => {
         setJunior(file)
     }
     return(
-        <form onSubmit={handleSubmit}>
-            <label>
-            學長姐資料:
-            <input type="file" onChange={handleSenior} />
-            </label>
-            <br />
-            <label>
-            學弟妹資料:
-            <input type="file" onChange={handleJunior} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <div >
+            <a href='https://hackmd.io/@BLGpkBRgRt--owIQaUv69Q/B1zGZZeDu'>
+                格式規定
+            </a>
+            <form onSubmit={handleSubmit}>
+                <label>
+                學長姐資料:
+                <input type="file" onChange={handleSenior} />
+                </label>
+                <br />
+                <label>
+                學弟妹資料:
+                <input type="file" onChange={handleJunior} />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export const Mail = () => {
+    
+    const [matchResult,setResult] = useState('')
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        if(matchResult==='') return
+        try{
+            const {data} = await study.study_matching({senior: matchResult,junior})
+            const url = window.URL.createObjectURL(new Blob([data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'file.xlsx'); //or any other extension
+            document.body.appendChild(link);
+            link.click();
+        }catch(e){
+            alert('error occur')
+        }
+    }
+    const handleMail = (e)=>{
+        const file = e.target.files[0]
+        setResult(file)
+    }
+    return(
+        <div >
+            自動寄信：寄送留學配對結果信件
+            <a href='https://hackmd.io/@BLGpkBRgRt--owIQaUv69Q/B1zGZZeDu'>
+                格式規定
+            </a>
+            <form onSubmit={handleSubmit}>
+                <label>
+                檔案:
+                <input type="file" onChange={handleMail} />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
